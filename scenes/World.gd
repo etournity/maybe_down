288cd3e3cf32e332065e32dcf9c8d799e633d10a
Julian Karhof <julian@karhof.net>
2023-07-08 14:06:25 +0200

    feat: add stage rotation

--- scenes/World.gd
+++ scenes/World.gd
@@ -2,20 +2,23 @@ extends Node3D
 
 var npc : RigidBody3D 
 var world: Node3D
-# Called when the node enters the scene tree for the first time.
+var camera: Camera3D
+var gravity: Vector3 = Vector3(0,-1,0)
+
 func _ready():
 	npc = get_node("Node3D/NPC")
 	world = get_node("World")
+	camera = get_node("Camera")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
+func _process(_delta):
 	if(Input.is_action_just_pressed("left")):
-		world.global_transform = npc.global_transform * Transform3D().rotated(Vector3(0,0,1), 10 * delta)
+		gravity += Vector3(0.1,0,0)
+		camera.rotate_z(0.01)
 	if(Input.is_action_just_pressed("right")):
-		world.global_transform = npc.global_transform * Transform3D().rotated(Vector3(0,0,1), -10 *delta)
-		
-	world.position = Vector3(0,0,0)
+		gravity += Vector3(-0.1,0,0)
+		camera.rotate_z(-0.01)
+	PhysicsServer3D.area_set_param(get_world_3d().space, PhysicsServer3D.AREA_PARAM_GRAVITY_VECTOR, gravity)
+
+
 	if(Input.is_action_just_pressed("quit")):
 		get_tree().quit()
