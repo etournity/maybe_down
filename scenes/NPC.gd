a30f234ccaf1ab6abe47eefcdf9e1d3fa6f71a4d
Julian Karhof <julian@karhof.net>
2023-07-09 17:05:36 +0200

    feat: add random funky sounds

--- scenes/NPC.gd
+++ scenes/NPC.gd
@@ -1,8 +1,12 @@
 extends RigidBody3D
 
-@export var sounds: Array[NoiseResource] = []
+@export var sounds: Array[AudioStream] = []
+
+var since_last = 0
+var rng = RandomNumberGenerator.new()
 
 func _process(delta):
+	since_last += 1
 	if (get_collision_mask_value(1) == false && linear_velocity.y < 0):
 		set_collision_mask_value(1, true)
 		set_transparency(1)
@@ -13,9 +17,23 @@ func _on_body_entered(body):
 		set_collision_mask_value(1, false)
 		set_transparency(0.2)
 	if (body.get_collision_layer_value(1) && body.has_method('play_random_sound')):
-		body.play_random_sound()
+		if(rng.randf() > 0.05):
+			body.play_random_sound()
+		else:
+			play_funky_sound()
 
 func set_transparency(value):
 	var override = get_child(0).get_child(0).get_surface_override_material(0)
 	override.albedo_color = Color(1,1,1,value)
 	get_child(0).get_child(0).set_surface_override_material(0, override)
+
+func play_funky_sound():
+	if (sounds.size() > 0 && since_last > 30):
+		since_last = 0
+		var player = AudioStreamPlayer.new()
+		player.stream = sounds.pick_random()
+		player.autoplay = true
+		player.bus = "Effects"
+		add_child(player)
+		await player.finished
+		player.queue_free()
