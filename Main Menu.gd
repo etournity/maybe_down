882fc570a23e24e63f47126e1e6b246d9edafbc4
Gabriel Gallardo Alarcon <pekena.larva@gmail.com>
2023-07-09 13:28:48 +0200

    Menu Music starts and stops when changing scene

--- Main Menu.gd
+++ Main Menu.gd
@@ -1,18 +1,20 @@
 extends Control
 
+var music_player: AudioStreamPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	music_player = $MenuMusic  # Assign the reference to the existing "Menu Music" AudioStreamPlayer node
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_play_pressed():
+	music_player.stop()  # Stop the playback of the "Menu Music" AudioStreamPlayer
 	hide()
 	Global.start_game()
+	
 
 func _on_options_pressed():
 	$Container.hide()
