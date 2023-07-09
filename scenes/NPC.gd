e2704ab3f92f37b6763e6d9f68a85e9030f3afa0
Felix <felix.luplow@code.berlin>
2023-07-09 03:22:30 +0200

    fix: no transparency to prevent lag spike

--- scenes/NPC.gd
+++ scenes/NPC.gd
@@ -3,13 +3,13 @@ extends RigidBody3D
 func _process(delta):
 	if (get_collision_mask_value(1) == false && linear_velocity.y < 0):
 		set_collision_mask_value(1, true)
-		set_transparency(0)
+#		set_transparency(0)
 
 func _on_body_entered(body):
 	if body.is_in_group("bed"):
 		apply_force(Vector3(0, body.velocity, 0))
 		set_collision_mask_value(1, false)
-		set_transparency(1)
+#		set_transparency(1)
 		
 
 func set_transparency(value):
