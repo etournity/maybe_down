8f415efa4657d7951aad38d37150c11bf37a9641
Julian Karhof <julian@karhof.net>
2023-07-09 03:26:40 +0200

    fix: transparency transition

--- scenes/NPC.gd
+++ scenes/NPC.gd
@@ -3,16 +3,16 @@ extends RigidBody3D
 func _process(delta):
 	if (get_collision_mask_value(1) == false && linear_velocity.y < 0):
 		set_collision_mask_value(1, true)
-#		set_transparency(0)
+		set_transparency(1)
 
 func _on_body_entered(body):
 	if body.is_in_group("bed"):
 		apply_force(Vector3(0, body.velocity, 0))
 		set_collision_mask_value(1, false)
-#		set_transparency(1)
+		set_transparency(0.2)
 		
 
 func set_transparency(value):
 	var override = get_child(0).get_child(0).get_surface_override_material(0)
-	override.transparency = value
+	override.albedo_color = Color(1,1,1,value)
 	get_child(0).get_child(0).set_surface_override_material(0, override)
